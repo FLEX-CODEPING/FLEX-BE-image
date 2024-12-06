3face0ef04d9bd4bdc77e0aaa72a0e32d7e4d3c2
joowojr <joowojr@gmail.com>
2024-12-06 13:28:33 +0900

    FLEX-330 ♻️ Refactor: redis 비동기 방식으로 수정
    <footer>
    - 관련: #12

--- app/infra/redis_config.py
+++ app/infra/redis_config.py
@@ -1,9 +1,11 @@
-import redis
+import redis.asyncio as redis
 from app.config.app_config import settings
 
-def redis_client():
-    return redis.StrictRedis(
+redis_client = redis.StrictRedis(
         host=settings.redis_host, 
         port=settings.redis_port, 
         db=settings.redis_db, 
         decode_responses=True)
+
+async def get_redis() -> redis.Redis:
+    return redis_client
